6006e8a1878966eae322f26fd19594908505a3ce
Martin Tint <martin.tint@mail.ee>
2023-10-19 16:25:04 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Logo" src="@/assets/eliis_logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Open Sans, sans-serif;
+}
+</style>
